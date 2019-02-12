d7e9db836d1f37f1745a1cab606d3b9ed0dabc26
Doug Clinton <98534+DougC@users.noreply.github.com>
2019-02-12 09:44:06 +0000

    [NGC-4765] Split the live service out to its own file

---
+++ app/uk/gov/hmrc/mobilestartup/services/LiveStartupService.scala
@@ -0,0 +1,70 @@
+package uk.gov.hmrc.mobilestartup.services
+import cats.Semigroup
+import cats.implicits._
+import javax.inject.Inject
+import play.api.Logger
+import play.api.libs.json.Json.{obj, _}
+import play.api.libs.json.{JsObject, JsString, JsValue}
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.mobilestartup.connectors.GenericConnector
+import uk.gov.hmrc.time.TaxYear
+
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.control.NonFatal
+
+class LiveStartupService @Inject()(connector: GenericConnector)(implicit ec: ExecutionContext) extends StartupService {
+
+  implicit val jsonObjectSemigroup: Semigroup[JsObject] = new Semigroup[JsObject] {
+    override def combine(x: JsObject, y: JsObject): JsObject = x ++ y
+  }
+
+  override def startup(nino: String, journeyId: Option[String])(implicit hc: HeaderCarrier): Future[JsObject] =
+    callService("helpToSave")(mhtsStartup) |+|
+      callService("taxCreditRenewals")(tcrStartup(journeyId)) |+|
+      callService("taxSummary")(taxSummaryStartup(nino, TaxYear.current.currentYear, journeyId))
+
+  private def buildJourneyQueryParam(journeyId: Option[String]): String =
+    journeyId.fold("")(id => s"?journeyId=$id")
+
+  private def logJourneyId(journeyId: Option[String]) =
+    s"Native Error - ${journeyId.fold("no Journey id supplied")(id => id)}"
+
+  private def callService(name: String)(f: => Future[Option[JsValue]]): Future[JsObject] =
+    f.map { result =>
+      val json: JsValue = result.getOrElse(obj())
+      obj(name -> json)
+    }
+
+  private def mhtsStartup(implicit hc: HeaderCarrier): Future[Option[JsValue]] =
+    connector
+      .doGet("mobile-help-to-save", "/mobile-help-to-save/startup", hc)
+      .map(Some(_))
+      .recover {
+        case NonFatal(e) =>
+          Logger.warn(s"""Exception thrown by "/mobile-help-to-save/startup", not returning any helpToSave result""", e)
+          None
+      }
+
+  private def tcrStartup(journeyId: Option[String])(implicit hc: HeaderCarrier): Future[Option[JsValue]] =
+    connector
+      .doGet("mobile-tax-credits-renewal", s"/income/tax-credits/submission/state/enabled${buildJourneyQueryParam(journeyId)}", hc)
+      .map(res => Some(JsObject(Seq("submissionsState" -> JsString((res \ "submissionsState").as[String])))))
+      .recover {
+        case NonFatal(e) =>
+          Logger.warn(
+            s"${logJourneyId(journeyId)} - Failed to retrieve TaxCreditsRenewals and exception is ${e.getMessage}! Default of submissionsState is error!")
+          Some(JsObject(Seq("submissionsState" -> JsString("error"))))
+      }
+
+  private def taxSummaryStartup(nino: String, year: Int, journeyId: Option[String])(implicit hc: HeaderCarrier): Future[Option[JsValue]] =
+    connector
+      .doGet("personal-income", s"/income/$nino/tax-summary/$year${buildJourneyQueryParam(journeyId)}", hc)
+      .map(Some(_))
+      .recover {
+        case ex: Exception =>
+          Logger.warn(s"${logJourneyId(journeyId)} - Failed to retrieve the tax-summary data and exception is ${ex.getMessage}!")
+          // An empty JSON object indicates failed to retrieve the tax-summary.
+          Some(obj())
+      }
+
+}
