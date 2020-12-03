c872b9693ab35e450fbb80c05f197500a9c1480d
JenniferMcMillan10 <34574866+JenniferMcMillan10@users.noreply.github.com>
2020-12-03 09:12:11 +0000

    [HMA-3873] Update sandbox data

--- app/uk/gov/hmrc/mobilestartup/controllers/SandboxStartupController.scala
+++ app/uk/gov/hmrc/mobilestartup/controllers/SandboxStartupController.scala
@@ -15,20 +15,50 @@
  */
 
 package uk.gov.hmrc.mobilestartup.controllers
+
 import javax.inject.Inject
-import play.api.libs.json.Json
-import play.api.mvc.{Action, AnyContent, ControllerComponents}
+import play.api.libs.json.Json.toJson
+import play.api.libs.json.{JsValue, Json}
+import play.api.mvc.{Action, AnyContent, BodyParser, ControllerComponents}
+import uk.gov.hmrc.api.controllers.HeaderValidator
 import uk.gov.hmrc.api.sandbox.FileResource
 import uk.gov.hmrc.mobilestartup.model.types.ModelTypes.JourneyId
 import uk.gov.hmrc.play.bootstrap.controller.BackendBaseController
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
-class SandboxStartupController @Inject() (val controllerComponents: ControllerComponents)(implicit ec: ExecutionContext)
-    extends BackendBaseController
-    with FileResource {
+class SandboxStartupController @Inject()(val controllerComponents: ControllerComponents)(implicit val ec: ExecutionContext)
+  extends BackendBaseController
+    with FileResource
+    with HeaderValidator {
 
-  def startup(journeyId: JourneyId): Action[AnyContent] = Action { _ =>
-    Ok(Json.parse(findResource("/sandbox/startup.json").getOrElse("{}")))
+  def startup(journeyId: JourneyId): Action[AnyContent] = {
+    validateAccept(acceptHeaderValidationRules).async { implicit request =>
+      val sandboxControl: Option[String] = request.headers.get("SANDBOX-CONTROL")
+
+      Future.successful {
+        sandboxControl match {
+          case Some("ERROR-401") => Unauthorized
+          case Some("ERROR-403") => Forbidden
+          case Some("ERROR-500") => InternalServerError
+          case Some("RENEWALS-OPEN") => Ok(readData("startup.json"))
+          case Some("RENEWALS-VIEW-ONLY") => Ok(readData("startup.json", renewalsStatus = "status_view_only"))
+          case Some("RENEWALS-CLOSED") => Ok(readData("startup.json", renewalsStatus = "closed"))
+          case Some("HTS-ENROLLED") => Ok(readData("startup.json"))
+          case Some("HTS-ELIGABLE") => Ok(readData("startup.json", htsStatus = "NotEnrolledButEligable"))
+          case Some("HTS-NOT-ENROLLED") => Ok(readData("startup.json", htsStatus = "NotEnrolled"))
+          case _ => Ok(readData("startup.json"))
+        }
+      }
+    }
   }
+
+  private def readData(resource: String, renewalsStatus: String = "open", htsStatus: String = "Enrolled") = {
+          findResource(s"/resources/mobilepayesummary/$resource")
+            .getOrElse(throw new IllegalArgumentException("Resource not found!"))
+            .replace("<RENEWALS_STATUS>", renewalsStatus)
+            .replace("<HTS_STATUS>", htsStatus)
+  }
+
+  override def parser: BodyParser[AnyContent] = controllerComponents.parsers.anyContent
 }
