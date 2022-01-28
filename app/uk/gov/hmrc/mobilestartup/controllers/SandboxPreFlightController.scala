9e645dbbb8c8381d112277a5c4ac1fc0f67abc46
aerodigi <54714520+aerodigi@users.noreply.github.com>
2022-01-28 11:50:17 +0000

    [HMA-5420] Update startup response for sandbox data with UTR section

--- app/uk/gov/hmrc/mobilestartup/controllers/SandboxPreFlightController.scala
+++ app/uk/gov/hmrc/mobilestartup/controllers/SandboxPreFlightController.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 HM Revenue & Customs
+ * Copyright 2022 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,10 +23,11 @@ import uk.gov.hmrc.api.sandbox.FileResource
 import uk.gov.hmrc.auth.core.retrieve.ItmpName
 import uk.gov.hmrc.domain.{Nino, SaUtr}
 import uk.gov.hmrc.mobilestartup.model.types.ModelTypes.JourneyId
-import uk.gov.hmrc.mobilestartup.services.{AnnualTaxSummaryLink, PreFlightCheckResponse}
+import uk.gov.hmrc.mobilestartup.services.{AnnualTaxSummaryLink, PreFlightCheckResponse, Utr}
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendBaseController
 import eu.timepit.refined.auto._
 import uk.gov.hmrc.auth.core.Enrolments
+import uk.gov.hmrc.mobilestartup.model.Activated
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -73,7 +74,7 @@ class SandboxPreFlightController @Inject() (
       toIV,
       Some(ItmpName(givenName = Some("Nia"), None, familyName = Some("Jackson"))),
       Some(AnnualTaxSummaryLink("/", "PAYE")),
-      None,
+      Some(Utr(Some(SaUtr("1234567890")), Activated)),
       Enrolments(Set.empty)
-    )
+      )
 }
