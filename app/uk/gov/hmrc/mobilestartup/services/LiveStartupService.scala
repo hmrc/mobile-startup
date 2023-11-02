e054ce72960eb73ebff37d2b2131775557f8610e
william-hatzar <22661741+william-hatzar@users.noreply.github.com>
2023-11-02 09:30:28 +0000

    Removal of feature switches from mobile-startup

--- app/uk/gov/hmrc/mobilestartup/services/LiveStartupService.scala
+++ app/uk/gov/hmrc/mobilestartup/services/LiveStartupService.scala
@@ -24,11 +24,6 @@ import scala.concurrent.{ExecutionContext, Future}
 
 class LiveStartupService @Inject() (
   connector:                                                                                                                        GenericConnector[Future],
-  @Named("feature.userPanelSignUp") userPanelSignUp:                                                                                Boolean,
-  @Named("feature.enablePushNotificationTokenRegistration") enablePushNotificationTokenRegistration:                                Boolean,
-  @Named("feature.paperlessAlertDialogs") enablePaperlessAlertDialogs:                                                              Boolean,
-  @Named("feature.paperlessAdverts") enablePaperlessAdverts:                                                                        Boolean,
-  @Named("feature.htsAdverts") enableHtsAdverts:                                                                                    Boolean,
   @Named("feature.annualTaxSummaryLink") enableAnnualTaxSummaryLink:                                                                Boolean,
   @Named("cbProofOfEntitlementUrl") cbProofOfEntitlementUrl:                                                                        Option[String],
   @Named("cbProofOfEntitlementUrlCy") cbProofOfEntitlementUrlCy:                                                                    Option[String],
@@ -48,20 +43,9 @@ class LiveStartupService @Inject() (
   @Named("niSummaryUrl") niSummaryUrl:                                                                                              Option[String],
   @Named("niContributionsUrl") niContributionsUrl:                                                                                  Option[String],
   @Named("otherTaxesDigitalAssistantUrl") otherTaxesDigitalAssistantUrl:                                                            Option[String],
-  @Named("otherTaxesDigitalAssistantUrlCy") otherTaxesDigitalAssistantUrlCy:                                                        Option[String],
-  @Named("feature.customerSatisfactionSurveys") enableCustomerSatisfactionSurveys:                                                  Boolean,
-  @Named("feature.findMyNinoAddToWallet") findMyNinoAddToWallet:                                                                      Boolean,
-  @Named("feature.disableYourEmploymentIncomeChart") disableYourEmploymentIncomeChart:                                              Boolean,
-  @Named("feature.disableYourEmploymentIncomeChartAndroid") disableYourEmploymentIncomeChartAndroid:                                Boolean,
-  @Named("feature.disableYourEmploymentIncomeChartIos") disableYourEmploymentIncomeChartIos:                                        Boolean,
-  @Named("feature.findMyNinoAddToGoogleWallet") findMyNinoAddToGoogleWallet:                                                          Boolean
+  @Named("otherTaxesDigitalAssistantUrlCy") otherTaxesDigitalAssistantUrlCy:                                                        Option[String]
 )(implicit ec:                                                                                                                      ExecutionContext)
     extends StartupServiceImpl[Future](connector,
-                                       userPanelSignUp,
-                                       enablePushNotificationTokenRegistration,
-                                       enablePaperlessAlertDialogs,
-                                       enablePaperlessAdverts,
-                                       enableHtsAdverts,
                                        enableAnnualTaxSummaryLink,
                                        cbProofOfEntitlementUrl,
                                        cbProofOfEntitlementUrlCy,
@@ -81,10 +65,4 @@ class LiveStartupService @Inject() (
                                        niSummaryUrl,
                                        niContributionsUrl,
                                        otherTaxesDigitalAssistantUrl,
-                                       otherTaxesDigitalAssistantUrlCy,
-                                       enableCustomerSatisfactionSurveys,
-                                       findMyNinoAddToWallet,
-                                       disableYourEmploymentIncomeChart,
-                                       disableYourEmploymentIncomeChartAndroid,
-                                       disableYourEmploymentIncomeChartIos,
-                                       findMyNinoAddToGoogleWallet)
+                                       otherTaxesDigitalAssistantUrlCy)
