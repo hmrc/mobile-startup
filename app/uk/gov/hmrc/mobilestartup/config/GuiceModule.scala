9079b7c7299a3eb90c8642cff11502d7b34e6158
JavedBeg-ac <181977665+JavedBeg-ac@users.noreply.github.com>
2026-09-03 14:38:56 +0100

    HMA-12492 revert changes for enableAdditional income flag

--- app/uk/gov/hmrc/mobilestartup/config/GuiceModule.scala
+++ app/uk/gov/hmrc/mobilestartup/config/GuiceModule.scala
@@ -129,6 +129,7 @@ class GuiceModule @Inject() (
     bindConfigBoolean("feature.enableChildBenefitMVP")
     bindConfigBoolean("feature.enableStudentLoanPlanTypeFive")
     bindConfigBoolean("feature.enableSACessation")
+    bindConfigBoolean("feature.enableAdditionalIncome")
     bindConfigBoolean("feature.enableWinterFuelPayment")
     bindConfigBoolean("feature.devEnableITSA")
     bindConfigBoolean("feature.enableTaxAndBenefitView")
